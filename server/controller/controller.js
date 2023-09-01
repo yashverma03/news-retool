6fee40f5461529fa35c6c8b079e22e43ef1bafbd
yashverma03 <vermayash2003@gmail.com>
2023-09-01 14:22:02 +0530

    added all news api

--- server/controller/controller.js
+++ server/controller/controller.js
@@ -1,16 +1,28 @@
+import axios from 'axios';
+
 export const getNews = async (req, res) => {
   try {
+    const apiKey = process.env.API_KEY;
+
     const { search, country } = req.query;
 
-    if (search) {
-      console.log('Search Query:', search);
+    let apiUrl = 'https://newsapi.org/v2/';
+
+    if (!search && !country) {
+      apiUrl += `everything?apiKey=${apiKey}&q=india`;
+    } else if (search && country) {
+      apiUrl += `everything?apiKey=${apiKey}&q=${search}&country=${country}`;
+    } else if (search) {
+      apiUrl += `everything?apiKey=${apiKey}&q=${search}`;
+    } else if (country) {
+      apiUrl += `top-headlines?apiKey=${apiKey}&country=${country}`;
+    } else {
+      return res.status(400).json({ error: 'Invalid request' });
     }
 
-    if (country) {
-      console.log('Country Query:', country);
-    }
+    const response = await axios.get(apiUrl);
 
-    res.send('Get all news working correctly');
+    res.status(200).json(response.data);
   } catch (error) {
     console.log(error);
     res.status(500).json({ error: 'Internal Server Error' });
